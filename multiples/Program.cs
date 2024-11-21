60dd3f5435b68db8167f2f356be07d49edd25bce
MaTERSS <146178004+MaTERSS@users.noreply.github.com>
2024-11-21 16:46:51 +0800

    Update Program.cs

--- multiples/Program.cs
+++ multiples/Program.cs
@@ -1,10 +1,11 @@
-﻿/*
+/*
  Дано N (10 ≤ N ≤ 25). 
 Найти количество чисел от 50 до 150 (включая эти числа), которые кратны N. 
 Операции деления (/, %) не использовать. А умножение не требуется. Посмотрите на задачу “Последовательность”
 Число N всего одно, его надо получить в нужном диапазоне. Хоть с помощью Random, хоть ввод пользователя.
  */
 using System;
+using System.Diagnostics.Metrics;
 using System.Text;
 
 namespace CSharplight
@@ -14,28 +15,20 @@ namespace CSharplight
         static void Main(string[] args)
         {            
             int numberFirst = 10;
-            int numberLast = 25;
+            int numberLast = 25;    
             Random rand = new Random();
             int randomNumber = rand.Next(numberFirst, numberLast);
             int firstNumber = 50;
             int lastNumber = 150; 
             int numberOfMultiples = 0;
-            int numbers;
-            int minuend;
 
-            for (numbers = firstNumber; numbers <= lastNumber; numbers++)
+            for (int i = 0; i <= lastNumber; i += randomNumber)
             {
-                minuend = numbers;
-                while (minuend >= randomNumber)
+                if (i >= firstNumber)
                 {
-                    minuend -= randomNumber;
+                numberOfMultiples++;
                 }
-
-                if (minuend == 0)
-                {
-                    numberOfMultiples++;
-                }
-            }
+            }           
 
             Console.WriteLine($"Количество чисел от 50 до 150, кратных {randomNumber}: {numberOfMultiples}");
         }        
